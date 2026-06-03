b21870ff30e32ecef0bc8c49d8c546da23dc8c4c
Rodrigo <rodrigo.machadoassis14@gmail.com>
2026-06-03 13:11:17 -0300

    linting

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,10 +1,10 @@
 class ArticlesController < ApplicationController
 
-    def show
-        @article = Article.find(params[:id])
-    end
+  def show
+    @article = Article.find(params[:id])
+  end
 
-    def index
-        @articles = Article.all
-    end
+  def index
+    @articles = Article.all
+  end
 end
