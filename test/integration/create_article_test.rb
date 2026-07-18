88dfddf6e9e26e4bb3526ae024279d87eabe9f43
Rodrigo <rodrigo.machadoassis14@gmail.com>
2026-07-18 19:49:32 -0300

    integration tests

---
+++ test/integration/create_article_test.rb
@@ -0,0 +1,34 @@
+require "test_helper"
+
+class CreateArticleTest < ActionDispatch::IntegrationTest
+
+  setup do
+    @user = User.create(username: "johndoe", email: "johndoe@example.com",
+                        password: "password")
+    sign_in_as(@user)
+  end
+
+  test "get new article form and create article" do
+    get "/articles/new"
+    assert_response :success
+    assert_difference 'Article.count', 1 do
+      post articles_path, params: { article: { title: "My First Article",
+                                               description: "This is the description of my first article" } }
+      assert_response :redirect
+    end
+    follow_redirect!
+    assert_response :success
+    assert_match "My First Article", response.body
+  end
+
+  test "get new article form and reject invalid article submission" do
+    get "/articles/new"
+    assert_response :success
+    assert_no_difference 'Article.count' do
+      post articles_path, params: { article: { title: "no", description: "short" } }
+    end
+    assert_match "errors", response.body
+    assert_select 'div.alert'
+    assert_select 'h4.alert-heading'
+  end
+end
